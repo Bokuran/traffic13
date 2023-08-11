6c7de9d01ddcb79d2dfc6a1d0c6d8771827ce2cf
Bokuran <141324043+Bokuran@users.noreply.github.com>
2023-08-11 16:45:34 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,21 @@
+var swiper1 = new Swiper('.swiper1', {
+  spaceBetween: 30,
+  navigation: {
+    nextEl: '.swiper-button-next1',
+    prevEl: '.swiper-button-prev1',
+  },
+});
+var swiper2 = new Swiper('.swiper2', {
+  spaceBetween: 30,
+  navigation: {
+    nextEl: '.swiper-button-next2',
+    prevEl: '.swiper-button-prev2',
+  },
+});
+var swiper3 = new Swiper('.swiper3', {
+  spaceBetween: 30,
+  navigation: {
+    nextEl: '.swiper3.swiper-button-next',
+    prevEl: '.swiper3.swiper-button-prev',
+  },
+});
